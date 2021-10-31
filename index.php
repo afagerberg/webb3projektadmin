0a2c46428b460de6715a8e5593d5c5bd3b6cebc8
afagerberg <alicefagerberg96@gmail.com>
2021-10-31 17:39:23 +0100

    small text clean

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ include("includes/config.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>afagerberg admin | Administera min digitala portfolio</title>
+    <title>afagerberg admin | Administrera min digitala portfolio</title>
     <link rel="stylesheet" href="css/main.css">
 
     <!-- favikon-->
